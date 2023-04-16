8fbbed48b11ea9aa232bcb66d7258d566c34c391
Cbw1981 <braxtonwilkey@gmail.com>
2023-04-16 12:21:45 -0400

    updated files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const fs = require('fs');
+
+var uniqid = require('uniqid');
+
+module.exports = (app) => {
+
+    // GET /api/notes should read the db.json file and return all saved notes as JSON.
+    app.get('/api/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
+  
+    // POST /api/notes should receive a new note to save on the request body, 
+    // add it to the db.json file, and then return the new note to the client. 
+    app.post('/api/notes', (req, res) => {
+      let db = fs.readFileSync('db/db.json');
+      db = JSON.parse(db);
+      res.json(db);
+      // creating body for note
+      let userNote = {
+        title: req.body.title,
+        text: req.body.text,
+        // creating unique id for each note
+        id: uniqid(),
+      };
+      // pushing created note to be written in the db.json file
+      db.push(userNote);
+      fs.writeFileSync('db/db.json', JSON.stringify(db));
+      res.json(db);
+  
+    });
+};     
