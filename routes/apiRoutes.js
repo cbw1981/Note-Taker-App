951892224ae7e4c14794fb8906a5689852bdd004
Cbw1981 <braxtonwilkey@gmail.com>
2023-04-16 13:04:56 -0400

    update server and routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,42 @@
 const express = require('express')
+const fs = require('fs')
+// fs allows to write files
 const router = express.Router()
 const path = require('path')
-const fs = require('fs');
+const uuid = require('../helpers/helpers');
+// handles encrypted data
+// randomUUID generates random value
 
-var uniqid = require('uniqid');
+let db = require('../db/db.json')
+// connects db.json file
 
-module.exports = (app) => {
+router.get('/notes', (req, res) => {
+    console.log(`${req.method} request recieved`)
+    let notes = JSON.parse(fs.readFileSync('./db/db.json'))
 
-    // GET /api/notes should read the db.json file and return all saved notes as JSON.
-    app.get('/api/notes', (req, res) => {
-      res.sendFile(path.join(__dirname, '../db/db.json'));
-    });
-  
-    // POST /api/notes should receive a new note to save on the request body, 
-    // add it to the db.json file, and then return the new note to the client. 
-    app.post('/api/notes', (req, res) => {
-      let db = fs.readFileSync('db/db.json');
-      db = JSON.parse(db);
-      res.json(db);
-      // creating body for note
-      let userNote = {
-        title: req.body.title,
-        text: req.body.text,
-        // creating unique id for each note
-        id: uniqid(),
-      };
-      // pushing created note to be written in the db.json file
-      db.push(userNote);
-      fs.writeFileSync('db/db.json', JSON.stringify(db));
-      res.json(db);
-  
-    });
-};     
+    res.status(200).json(notes)
+    console.log('received notes')
+});
+
+router.post('/notes', (req, res) => {
+    console.log(`${req.method} request recieved`)
+
+    const {title, text} = req.body;
+
+    const newNote = {
+        note_id: uuid(),
+        title,
+        text,
+    }
+    let notes = JSON.parse(fs.readFileSync('./db/db.json'))
+
+    notes.push(newNote)
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2));
+   
+    res.json(notes);
+    console.log('note created')
+
+});
+
+module.exports = router;
